c01c97ccff34819be97f1f6f11988ac457f37f67
Fabi Kadir KURTULUŞ <l1516001@std.yildiz.edu.tr>
2019-11-11 11:55:03 +0300

    Add files via upload

---
+++ Project Startech/ZedGraphGuncelleyici.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZedGraphKutuphanesi;
+using RibbonKutuphanesi.Kontrolculer;
+
+namespace Project_Startech
+{
+    public class ZedGraphGuncelleyici
+    {
+        BackgroundWorker Backgroundworker = new BackgroundWorker();
+        ZedGraphKontrol[] GrafikKontrolleri;
+        SeriPortKontrolcusu SeriportKontrolcusu;
+        Panel panel_main;
+
+        public ZedGraphGuncelleyici(SeriPortKontrolcusu seriportKontrolcusu, ZedGraphKontrol[] grafikKontrolleri, Panel panel)
+        {
+            panel_main = panel;
+
+            Backgroundworker.DoWork += ZedGraphlariGuncelle;
+
+            SeriportKontrolcusu = seriportKontrolcusu;
+            SeriportKontrolcusu.SeriPortAcildi += GuncellemeyiBasla;
+            seriportKontrolcusu.SeriPortKapandi += GuncellemeyiBitir;
+
+            GrafikKontrolleri = grafikKontrolleri;
+        }
+        void GuncellemeyiBasla(object sender, EventArgs e)
+        {
+            Backgroundworker.RunWorkerCompleted += GuncellemeTamam;
+            Backgroundworker.RunWorkerAsync();
+        }
+        void ZedGraphlariGuncelle(object sender, DoWorkEventArgs e)
+        {
+            int zedgraph_indis = panel_main.Controls[0].TabIndex;
+            for (int i = (zedgraph_indis < 6) ? 0 : 6; i < SeriportKontrolcusu.VeriSayisi; i++)
+            {
+                if (!GrafikKontrolleri[i].GecmisiIncele)
+                {
+                    try
+                    {
+                        double sonDeger = GrafikKontrolleri[i].VeriListesi.SonXVerisi;
+                        GrafikKontrolleri[i].X_EkseniniOlceklendir(sonDeger);
+                    }
+                    catch
+                    {
+                    }
+                }
+            }
+            SeriportKontrolcusu.GercekZamanDegisiklikKontrol();
+            Thread.Sleep(1);
+        }
+        void GuncellemeTamam(object sender, RunWorkerCompletedEventArgs e) => Backgroundworker.RunWorkerAsync();
+        void GuncellemeyiBitir(object sender, EventArgs e) => Backgroundworker.RunWorkerCompleted -= GuncellemeTamam;
+    }
+}
